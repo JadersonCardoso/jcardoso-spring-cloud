f9d9697f82d0135a7cfaa8b53e73f12bb45ac287
jaderson.brandao <jaderson.brandao@gmail.com>
2021-10-23 15:29:26 -0300

    primeiro commit

---
+++ greeting-service/src/main/java/br/com/jcardoso/controllers/GreetingController.java
@@ -0,0 +1,26 @@
+package br.com.jcardoso.controllers;
+
+import br.com.jcardoso.configuration.GreetingConfiguration;
+import br.com.jcardoso.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "%s, %s";
+    private final AtomicLong couter = new AtomicLong();
+
+    @Autowired
+    private GreetingConfiguration configuration;
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name ) {
+        if (name.isEmpty()) name = configuration.getDefaultValue();
+        return new Greeting(couter.incrementAndGet(), String.format(template, configuration.getGreeting(), name));
+    }
+}
