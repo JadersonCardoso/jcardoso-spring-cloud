73f8bbd76061b952897ba36377b9c89c07b3961e
jaderson.brandao <jaderson.brandao@gmail.com>
2021-10-24 15:20:36 -0300

    Consume cambio-service with RestTemplate and Feign Client

--- book-service/src/main/java/br/com/jcardoso/controller/BookController.java
+++ book-service/src/main/java/br/com/jcardoso/controller/BookController.java
@@ -1,13 +1,18 @@
 package br.com.jcardoso.controller;
 
 import br.com.jcardoso.model.Book;
+import br.com.jcardoso.proxy.CambioProxy;
 import br.com.jcardoso.repository.BookRepository;
+import br.com.jcardoso.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -19,6 +24,9 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @Autowired
+    private CambioProxy proxy;
+
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id,
                          @PathVariable("currency") String cureency) {
@@ -26,10 +34,37 @@ public class BookController {
         var book = repository.getById(id);
         if (book == null) throw new RuntimeException("Book not found");
 
+        var cambio = proxy.getCambio(book.getPrice(), "USD", cureency);
+
         var port = environment.getProperty("local.server.port");
-        book.setEnvironment(port);
+        book.setEnvironment(port + "FEIGN");
+        book.setPrice(cambio.getConvertedValue());
         return book;
     }
+
+//    @GetMapping(value = "/{id}/{currency}")
+//    public Book findBook(@PathVariable("id") Long id,
+//                         @PathVariable("currency") String cureency) {
+//
+//        var book = repository.getById(id);
+//        if (book == null) throw new RuntimeException("Book not found");
+//
+//        HashMap<String, String> params = new HashMap<>();
+//        params.put("amount", book.getPrice().toString());
+//        params.put("from", "USD");
+//        params.put("to", cureency);
+//
+//        var response =  new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+//                Cambio.class, params);
+//
+//        var cambio = response.getBody();
+//
+//        var port = environment.getProperty("local.server.port");
+//        book.setEnvironment(port);
+//        book.setPrice(cambio.getConvertedValue());
+//        return book;
+//    }
+
 
 
 }
