0ef894bb5261a787470bf23d8bac72c45055ab25
jaderson.brandao <jaderson.brandao@gmail.com>
2021-10-24 11:01:51 -0300

    Created Cambio Service and integration with Flyway

---
+++ cambio-service/src/main/java/br/com/jcardoso/CambioServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcardoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambioServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CambioServiceApplication.class, args);
+    }
+
+}
