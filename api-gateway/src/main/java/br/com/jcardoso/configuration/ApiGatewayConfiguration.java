a9c28bb3735ebcaca9827dc5bf0b0087ca66a4ed
jaderson.brandao <jaderson.brandao@gmail.com>
2021-11-02 20:56:23 -0300

    Configuração SpringDoc OpenAPI UI no API Gateway

--- api-gateway/src/main/java/br/com/jcardoso/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/br/com/jcardoso/configuration/ApiGatewayConfiguration.java
@@ -5,22 +5,22 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ApiGatewayConfiguration {
 
-    @Bean
-    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(p -> p.path("/get")
-                    .filters(f -> f.addRequestHeader("Hello", "World")
-                                   .addRequestParameter("Hello", "World"))
-                    .uri("http://httpbin.org:80"))
-                .route(p -> p.path("/cambio-service/**")
-                        .uri("lb://cambio-service/**"))
-                .route(p -> p.path("/book-service/**")
-                        .uri("lb://book-service/**"))
-                .build();
-    }
+//    @Bean
+//    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p.path("/get")
+//                    .filters(f -> f.addRequestHeader("Hello", "World")
+//                                   .addRequestParameter("Hello", "World"))
+//                    .uri("http://httpbin.org:80"))
+//                .route(p -> p.path("/cambio-service/**")
+//                        .uri("lb://cambio-service/**"))
+//                .route(p -> p.path("/book-service/**")
+//                        .uri("lb://book-service/**"))
+//                .build();
+//    }
 }
 /*
 Function<PredicateSpec, Buildable<Route>> function =
