2b3951df5701fe39ab4a375f024fdffb416ed067
jaderson.brandao <jaderson.brandao@gmail.com>
2021-11-02 20:57:02 -0300

    Configuração SpringDoc OpenAPI UI no API Gateway

--- book-service/src/main/java/br/com/jcardoso/controller/BookController.java
+++ book-service/src/main/java/br/com/jcardoso/controller/BookController.java
@@ -3,6 +3,8 @@ package br.com.jcardoso.controller;
 import br.com.jcardoso.model.Book;
 import br.com.jcardoso.proxy.CambioProxy;
 import br.com.jcardoso.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -23,6 +26,7 @@ public class BookController {
     @Autowired
     private CambioProxy proxy;
 
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id,
                          @PathVariable("currency") String cureency) {
--- book-service/src/main/java/br/com/jcardoso/controller/FooBarController.java
+++ book-service/src/main/java/br/com/jcardoso/controller/FooBarController.java
@@ -4,6 +4,8 @@ import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Tag(name = "Foo bar endpoint")
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
@@ -18,6 +21,7 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
+    @Operation(summary = "Foo Bar")
 //    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
 //    @CircuitBreaker(name = "foo-bar", fallbackMethod = "fallbackMethod")
 //    @RateLimiter(name = "default")
